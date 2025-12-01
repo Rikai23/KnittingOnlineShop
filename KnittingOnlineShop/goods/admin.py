33a1271ffbaac93c734af7f3a45147aa6ad4e2d0
Rikai23 <andrei.rihard.99@yandex.ru>
2025-12-01 22:40:23 +0300

    Созданы модели товаров и категорий

--- KnittingOnlineShop/goods/admin.py
+++ KnittingOnlineShop/goods/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Categories, Products, ProductImage
+
+admin.site.register(Categories)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]  # вот здесь привязываем инлайн к продукту
--- KnittingOnlineShop/goods/models.py
+++ KnittingOnlineShop/goods/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в процентах')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Products, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='goods_images', verbose_name="Изображение")
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
+
+    def __str__(self):
+        return f"{self.product.name} - изображение"
